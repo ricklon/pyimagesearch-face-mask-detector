3dc232b904319943cbe8fb6718cfbf5e58f9154c
Rick Anderson <rick.rickanderson@gmail.com>
2020-07-05 13:55:12 -0400

    Update detect_mask_image.py

--- detect_mask_image.py
+++ detect_mask_image.py
@@ -98,6 +98,7 @@ for i in range(0, detections.shape[2]):
 
 # show the output image
 img_name = args["image"].split(".")
-cv2.imwrite(args[img_name[0]+ "_output.png", image) 
-cv2.imshow("Output", image)
-cv2.waitKey(0)
+
+cv2.imwrite(img_name[0] + "_output.png", image) 
+#cv2.imshow("Output", image)
+#cv2.waitKey(0)
